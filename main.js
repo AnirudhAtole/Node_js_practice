a67adf317779e203821d4e7a784a98e8b503ddaf
AnirudhAtole <anirudhatole@gmail.com>
2023-02-27 15:20:54 +0530

    writing and reading from a file

--- main.js
+++ main.js
@@ -1,19 +1,34 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
     res.setHeader('Content-Type','text/html');
+    const url = req.url;
+    const method = req.method;
+    const name = fs.readFileSync('name.txt',{encoding:'utf-8'});
+    if(req.url === '/'){
 
-    if(req.url === '/home'){
-        res.write('<html><head>My first page</head> <body> <h1> Welcome to home </h1> </body>  </html>');
-        res.end();
+        res.write('<html>');
+        res.write('<head><title> Enter the message </title> </head>');
+        res.write(`<body> <h1>${name}</h1> <form action = "/message" method ="POST" > <input type ="text" name ="message"> <button type = "submit">Send</button> </body>`);
+        res.write('</html>');
+        return res.end();
     }
-    else if(req.url === '/about'){
-        res.write('<html><head>My first page</head> <body> <h1> Welcome to about us page </h1> </body>  </html>');
-        res.end();
-    }
-    else if(req.url === '/node'){
-        res.write('<html><head>My first page</head> <body> <h1> Welcome to my Node Js project </h1> </body>  </html>');
-        res.end();
+    if(url === "/message" && method === "POST"){
+        const body = [];
+        req.on('data', (chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end',()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message  = parsedBody.split('=')[1];
+            fs.writeFileSync('name.txt',message);
+        })
+        fs.writeFileSync('name.txt','Dummy');
+        res.statusCode = 302;
+        res.setHeader('Location','/')
+        return res.end();
     }
     
 });
