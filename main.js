10dd6ab43b7591a144addc1f527e2934a61d3c51
AnirudhAtole <anirudhatole@gmail.com>
2023-02-27 09:31:31 +0530

    arrow functn and obe recaped

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+
+const product = (param1 , param2) => param1 * param2;
+
+console.log(product(3,3));
+
+const student ={
+    name : "Manish",
+    greet(){
+        console.log(`My name is ${this.name}`);
+    }
+}
+
+student.greet()
