3d02edfb0b4caeeeb4d5dedc2439d7268aeb3459
AnirudhAtole <anirudhatole@gmail.com>
2023-02-27 14:30:27 +0530

    req/res

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    console.log("My name is Anirudh");
-})
+    res.setHeader('Content-Type','text/html');
+
+    if(req.url === '/home'){
+        res.write('<html><head>My first page</head> <body> <h1> Welcome to home </h1> </body>  </html>');
+        res.end();
+    }
+    else if(req.url === '/about'){
+        res.write('<html><head>My first page</head> <body> <h1> Welcome to about us page </h1> </body>  </html>');
+        res.end();
+    }
+    else if(req.url === '/node'){
+        res.write('<html><head>My first page</head> <body> <h1> Welcome to my Node Js project </h1> </body>  </html>');
+        res.end();
+    }
+    
+});
+
 
 server.listen(4000);
+
