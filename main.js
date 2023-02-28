c1f5e89d7bec1e7c3bc860e8301de55bbe07683c
AnirudhAtole <anirudhatole@gmail.com>
2023-02-28 16:18:39 +0530

    used bodyparser

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 const http = require('http');
-
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.use((req,res,next) =>{
     console.log("Hello world");
     next(); // this allow requst to continue next line
 })
 
-app.use((req,res,next) =>{
-    console.log("Hello world 2");
+
+app.use('/add',(req,res,next) =>{
+    res.send('<form action ="/product" method ="POST" > <input type = "text" name="title"> <input type = "text" name="title2"><button type = "submit"> Add product </button></form>');
+})
+
+app.post('/product',(req,res ,next) =>{
+    console.log(req.body)
+    res.redirect('/')
+})
+
+app.use('/',(req,res) =>{
     res.send({key1:24})
 })
 
